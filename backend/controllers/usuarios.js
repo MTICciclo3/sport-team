36696d20fd13f930c3f886d8e348a5ad507409b0
Jhon Zafra <66529211+JhonZafra@users.noreply.github.com>
2021-10-20 23:08:16 -0500

    Delete backend directory

--- backend/controllers/usuarios.js
+++
@@ -1,8 +0,0 @@
-const Usuario = require('../models/usuarios');
-
-exports.getUsuarios = (req, res) => {
-    Usuario.find().then((usuarioResults) => {
-        res.status(200).json(usuarioResults) 
-    })
-    
-};
--- backend/models/usuarios.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const usuarioSchema = mongoose.Schema({
-    email: { type: String, required: true},
-    activo: { type: Boolean},
-    nombre: { type: String, required: true},
-});
-
-module.exports = mongoose.model("Usuario", usuarioSchema)
